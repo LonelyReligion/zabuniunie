cbd2c62601f0ce628781b8487505dfed60b9bd1b
flxgttr <agatazbirek@outlook.com>
2024-04-19 22:54:07 +0200

    probuje ogarnac ocb wgl

--- nadzaba/skrzek.py
+++ nadzaba/skrzek.py
@@ -1,9 +1,14 @@
 import gymnasium as gym
+import numpy
+
 env = gym.make("ALE/Frogger-v5", render_mode="human")
+#render_module: human, rgb_array, ansi, rgb_array_list, ansi_list 
+
 observation, info = env.reset()
 
 for _ in range(1000):
     action = env.action_space.sample()  # agent policy that uses the observation and info
+                                        # akcja tutaj moze byc liczba 0-4
     observation, reward, terminated, truncated, info = env.step(action)
 
     if terminated or truncated:
---
+++ nadzaba/zaba_agent.py
@@ -0,0 +1,10 @@
+class zaba_agent:
+    def __init__( #inicjalizacja
+        self
+        #listy prawdopodobienstw dla konkretnych przypadkow
+    ):
+        #przypisanie
+        pass
+
+    def pobierz_akcje(self): #co robimy + informacje ze srodowiska
+        return 1 #jakas akcja z najwiekszym prawdopodobienstwem
