34dd6dd1a0abc16ccc01e87d39c4ff6a2678b31d
flxgttr <agatazbirek@outlook.com>
2024-04-24 15:20:56 +0200

    mamy slepa zabe z losowym mozgiem

--- nadzaba/skrzek.py
+++ nadzaba/skrzek.py
@@ -10,17 +10,18 @@ def run():
     print(state)
     terminated = False
     truncated = False
-    #1 rzeka/lad 
-    #5 rzeka
-    #stop
-    #pięć pasów
-    #stop
-    #wykorzystac same 0 w state[0] do okreslania dlugosci pól:3!
+    # kolor ramki: [82, 126, 45]
+    # kolor żaby: [110, 156, 66]
+    # kolory aut:
+    # kolory liści (?):
+    # kolory kłód: 
+    # 13 wysokosc
+    # 8 + 1 + 8 = 19 szerokosc
     i = 0;
     zabcia = zaba_agent.zaba_agent()
     while(not terminated and not truncated): #na potrzeby testu, jedna zaba do smierci lub znudzenia;)
         i+=1
-        if i % 2 == 1:
+        if i % 6 <= 3:
             action = zabcia.pobierz_akcje(state[0])
         else:
             #konieczne, poniewaz symulujemy przyciski - jesli akcja sie powtorzy zostanie wykonanana tylko raz 
--- nadzaba/zaba_agent.py
+++ nadzaba/zaba_agent.py
@@ -8,35 +8,63 @@ class zaba_agent:
         #1
         #2  X
         #3
-        a1chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), #wartości domyślne dla sierot - losowe
+        chances = [
+            #a1
+            [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), #wartości domyślne dla sierot - losowe
                      d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d], #:= pozwala na przypisywanie wartości w wyrażeniach!
-        a2chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+            #a2
+            [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
                      d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
-        a3chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+            #a3
+            [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
                      d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
-        b1chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+            #b1
+            [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
                      d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
-        b3chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+            #b3
+            [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
                      d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
-        c1chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+            #c1
+            [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
                      d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
-        c2chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+            #c2
+            [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
                      d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
-        c3chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+            #c3
+            [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
                      d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d]
+        ]
     ):
-        self.a1chances = a1chances
-        self.a2chances = a2chances
-        self.a3chances = a3chances
-        self.b1chances = b1chances
-        self.b3chances = b3chances
-        self.c1chances = c1chances
-        self.c2chances = c2chances
-        self.c3chances = c3chances
+        self.chances = chances
+
+        self.pozycjax = 8
+        self.pozycjay = 0
 
         self.fitness = 0 #wynik inicjowany 0
         pass
 
     def pobierz_akcje(self, obserwacja): #co robimy + informacje ze srodowiska
         #self.fitness jest rowny pozycji y
-        return 1 #jakas akcja z najwiekszym prawdopodobienstwem
+        self.fitness = self.pozycjay
+        #rozpoznajemy akcje
+        noEvent = 1
+        print("Zarejestrowano zdarzenie " + str(noEvent) + "\n" + str(self.chances[noEvent]))
+        #kumulowanie prawdopodobienstwa
+        skumulowane = self.chances.copy() #pamietaj domyslnie kopiuje sie ;) wskazniki
+        for i in range(0, len(skumulowane[noEvent]) - 1): #dla kazdego kierunku
+            skumulowane[i] = self.chances[i].copy() #pamietaj domyslnie kopiuje sie ;) wskazniki
+            if(i == 0):
+                pass
+            else:
+                skumulowane[noEvent][i] = skumulowane[noEvent][i] + skumulowane[noEvent][i-1]
+
+        #losujemy akcje
+        wylosowana_wartosc = random.random()
+        for i in range(0, len(skumulowane[noEvent]) - 1): #dla kazdego kierunku
+            if(wylosowana_wartosc <= skumulowane[noEvent][i]):
+                print("Wylosowano akcje " + str(i))
+                return i
+
+
+        return 3 #jakas akcja z najwiekszym prawdopodobienstwem
+
