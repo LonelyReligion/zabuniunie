ca5631f466a42fa48d8ba8830df4cac8d1d73836
flxgttr <agatazbirek@outlook.com>
2024-04-24 11:29:39 +0200

    losowy mózg żaby i myślenie o observation_space! bierzemy tez akcje z agenta

--- nadzaba/skrzek.py
+++ nadzaba/skrzek.py
@@ -1,34 +1,33 @@
 import gymnasium as gym
-import numpy
+import numpy #moze do generowania seeda?
+import zaba_agent
 
 def run():
-    env = gym.make("ALE/Frogger-v5", render_mode="human")
+    env = gym.make("ALE/Frogger-v5", render_mode="human", obs_type="rgb")
     #render_module: human, rgb_array, ansi, rgb_array_list, ansi_list 
     print(env.observation_space)
-
-    #low high shape(x, y i rgb:)) dtype
-    #coś, x, y, rgb
     state = env.reset()
+    print(state)
     terminated = False
     truncated = False
-    #1 rzeka/lad
+    #1 rzeka/lad 
     #5 rzeka
     #stop
     #pięć pasów
     #stop
+    #wykorzystac same 0 w state[0] do okreslania dlugosci pól:3!
     i = 0;
+    zabcia = zaba_agent.zaba_agent()
     while(not terminated and not truncated): #na potrzeby testu, jedna zaba do smierci lub znudzenia;)
         i+=1
         if i % 2 == 1:
-            action = 1 #env.action_space.sample()
+            action = zabcia.pobierz_akcje(state[0])
         else:
             #konieczne, poniewaz symulujemy przyciski - jesli akcja sie powtorzy zostanie wykonanana tylko raz 
             #mogloby to zaburzyc proces uczenia sie imo
             action = 0
         new_state, reward, terminated, truncated, _ = env.step(action)
         state = new_state
-        if terminated or truncated:
-            state = env.reset()
     env.close()
 
 if __name__ == '__main__':
--- nadzaba/zaba_agent.py
+++ nadzaba/zaba_agent.py
@@ -1,3 +1,5 @@
+import random
+
 class zaba_agent:
     def __init__( #inicjalizacja
         self,
@@ -6,14 +8,22 @@ class zaba_agent:
         #1
         #2  X
         #3
-        a1chances,
-        a2chances,
-        a3chances,
-        b1chances,
-        b3chances,
-        c1chances,
-        c2chances,
-        c3chances
+        a1chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), #wartości domyślne dla sierot - losowe
+                     d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d], #:= pozwala na przypisywanie wartości w wyrażeniach!
+        a2chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+                     d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
+        a3chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+                     d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
+        b1chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+                     d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
+        b3chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+                     d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
+        c1chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+                     d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
+        c2chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+                     d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d],
+        c3chances = [a := random.random(), b:= random.uniform(0.0, 1-a), c:= random.uniform(0.0, 1-a-b), 
+                     d:= random.uniform(0.0, 1-a-b-c), 1-a-b-c-d]
     ):
         self.a1chances = a1chances
         self.a2chances = a2chances
@@ -23,9 +33,10 @@ class zaba_agent:
         self.c1chances = c1chances
         self.c2chances = c2chances
         self.c3chances = c3chances
-        self.fitness = 0 #wynik inicjalizowany 0
+
+        self.fitness = 0 #wynik inicjowany 0
         pass
 
-    def pobierz_akcje(self): #co robimy + informacje ze srodowiska
+    def pobierz_akcje(self, obserwacja): #co robimy + informacje ze srodowiska
         #self.fitness jest rowny pozycji y
         return 1 #jakas akcja z najwiekszym prawdopodobienstwem
